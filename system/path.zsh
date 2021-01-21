30e6776d589965964b8dd7884a2a0f67a2512a81
Peter Major <pmajor@sankynet.com>
2021-01-21 11:54:09 -0500

    Add some aliases

--- system/path.zsh
+++ system/path.zsh
@@ -1,1 +1,1 @@
-export PATH="$ZSH/bin:/usr/local/bin:/usr/local/sbin:/usr/local/aws/bin:$PATH"
+export PATH="$ZSH/bin:/usr/local/bin:/usr/local/sbin:/usr/local/aws/bin:~/Library/Python/3.7/bin:$PATH"
